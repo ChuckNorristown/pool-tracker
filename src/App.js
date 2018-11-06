4cda8eeb1257998dffc2a160752defe1ddaa524a
Sean Shields <shields@PrintFly-MacBook-D002.local>
2018-11-05 21:35:11 -0500

    adding basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SimpleStorage from "react-simple-storage";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      addResult: "",
+      list: []
+    };
+  }
+  updateInput(key, value) {
+    // update react state
+    this.setState({ [key]: value });
+  }
+
+
+
+
+
+  addPlayer() {
+    // create a new item with unique id
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+    // copy current list of items
+    const list = [...this.state.list];
+    // add the new item to the list
+    list.push(newItem);
+    // update state with new list, reset the new item input
+    this.setState({
+      list,
+      newItem: ""
+    });
+  }
+
+  addResult() {
+    // create a new item with unique id
+    const addResult = {
+      id: 1 + Math.random(),
+      value: this.state.addResult.slice()
+    };
+    // copy current list of items
+    const list = [...this.state.list];
+    // add the new item to the list
+    list.push(addResult);
+    // update state with new list, reset the new item input
+    this.setState({
+      list,
+      addResult: ""
+    });
+  }
+
+
+
+
+  deleteItem(id) {
+    // copy current list of items
+    const list = [...this.state.list];
+    // filter out the item being deleted
+    const updatedList = list.filter(item => item.id !== id);
+
+    this.setState({ list: updatedList });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div>
+        <SimpleStorage parent={this} />
+        <div
+          style={{
+            padding: 50,
+            textAlign: "left",
+            maxWidth: 500,
+            margin: "auto"
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Enter Player"
+            value={this.state.newItem}
+            onChange={e => this.updateInput("newItem", e.target.value)}
+          />
+          <button
+            onClick={() => this.addPlayer()}
+            disabled={!this.state.newItem.length}
           >
-            Learn React
-          </a>
-        </header>
+            &#43; Add
+          </button>
+          <div
+            style={{
+              padding: 50,
+              textAlign: "left",
+              maxWidth: 500,
+              margin: "auto"
+            }}
+          >
+            <ul>
+              {this.state.list.map(item => {
+                return (
+                  <li key={item.id}>
+                    {item.value}
+                    <br />
+                    <button onClick={() => this.deleteItem(item.id)}>
+                      Delete
+                    </button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
