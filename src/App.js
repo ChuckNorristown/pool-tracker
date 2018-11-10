9b829221daad0863969d5520583fba5c3e5909b1
Sean Shields <shields@PrintFly-MacBook-D002.local>
2018-11-10 09:12:32 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -38,58 +38,35 @@ class App extends Component {
     return (
       <div>
         <SimpleStorage parent={this} />
-        <input
-          type="text"
-          placeholder="Enter Player"
-          value={this.state.newPlayer}
-          onChange={e => this.updateInput("newPlayer", e.target.value)}
-        />
-        <button
-          onClick={() => this.addPlayer()}
-          disabled={!this.state.newPlayer.length}
-        >
-          &#43; Add
-        </button>
-        <div
-          style={{
-            padding: 50,
-            textAlign: "left",
-            maxWidth: 500,
-            margin: "auto"
-          }}
-        >
-          <ul>
-            {this.state.list.map(item => {
-              return (
-                <li key={item.id}>
-                  {item.value}
-                  <label>
-                    <br />
-                    <input
-                      type="number"
-                      min="0"
-                      placeholder="please enter value"
-                    />{" "}
-                    wins
-                  </label>
-                  <br />
-                  <label>
-                    <input
-                      type="number"
-                      min="0"
-                      placeholder="please enter value"
-                    />{" "}
-                    losses
-                  </label>
-                  <br />
-                  <button onClick={() => this.deleteItem(item.id)}>
-                    Delete
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        <form>
+          <label>
+            <input type="text" required value={this.state.newPlayer} onChange={e => this.updateInput("newPlayer", e.target.value)} />
+            <div className="label-text">Enter Player</div>
+          </label>
+          <button onClick={() => this.addPlayer()} disabled={!this.state.newPlayer.length} >
+            Submit
+          </button>
+        </form>
+        <ul>
+          {this.state.list.map(item => {
+            return (
+              <li key={item.id}>
+                {item.value}
+                <label>
+                  <input type="number" min="0" />
+                  <div className="label-text">wins</div>
+                </label>
+                <label>
+                  <input type="number" min="0" />
+                  <div className="label-text">losses</div>
+                </label>
+                <button onClick={() => this.deleteItem(item.id)}>
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
