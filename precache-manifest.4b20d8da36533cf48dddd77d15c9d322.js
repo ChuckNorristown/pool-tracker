5e02dcc59a5edeb751b92c99e594b95aea6e6e6b
Sean Shields <shields@PrintFly-MacBook-D002.local>
2018-11-11 19:21:28 -0500

    Updates

---
+++ precache-manifest.4b20d8da36533cf48dddd77d15c9d322.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0016cdb87a53815f8426",
+    "url": "/pool-tracker/static/css/main.cab5fa11.chunk.css"
+  },
+  {
+    "revision": "0016cdb87a53815f8426",
+    "url": "/pool-tracker/static/js/main.0016cdb8.chunk.js"
+  },
+  {
+    "revision": "18109dcc616910518996",
+    "url": "/pool-tracker/static/js/1.18109dcc.chunk.js"
+  },
+  {
+    "revision": "b1eb5ba52f8dd1648738",
+    "url": "/pool-tracker/static/js/runtime~main.b1eb5ba5.js"
+  },
+  {
+    "revision": "46548dec97922257007b595dff921145",
+    "url": "/pool-tracker/index.html"
+  }
+];
